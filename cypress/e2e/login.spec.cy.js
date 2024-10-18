d9e8fd64ed1b10a355b1354c5d697763d9e84afa
gabrielucasantos <gabrielucasdosantos@gmail.com>
2024-10-18 16:17:13 -0300

    Primeiro commit

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,17 @@
+describe('Orange HRM Tests', () => {
+  it('Login - Success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin123')
+    cy.get("button[type='submit']").click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+  })
+  it('Login - Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Testfail')
+    cy.get("[name='password']").type('Senhafail')
+    cy.get("button[type='submit']").click()
+    cy.get("[role='alert']")
+  }) 
+})
